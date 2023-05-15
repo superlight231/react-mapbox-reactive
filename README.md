8e7f291e7356fe3b72875225dea8555ec19c03c8
Austin Lee <austinl28312@gmail.com>
2023-05-15 11:47:06 -0500

    chore: scaffold vite + react + typescript project

---
+++ README.md
@@ -0,0 +1,5 @@
+# react-mapbox-reactive
+
+Experimenting with driving a Mapbox GL map from React state.
+
+More to come.
---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ padding: 24 }}>
+      <h1>react-mapbox-reactive</h1>
+      <p>Project scaffold — map integration coming soon.</p>
+    </div>
+  )
+}
+
+export default App
